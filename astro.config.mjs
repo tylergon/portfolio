65379c1cb5e12f73b5b47d925ce84db061633068
Tyler Goncalves <tylergoncalves@shaw.ca>
2026-03-07 09:14:27 -0800

    Updated settings for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,9 +11,9 @@ import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "https://tylergon.github.io",
-  base: config.site.base_path ? config.site.base_path : "/portfolio",
-  trailingSlash: config.site.trailing_slash ? "always" : "never",
+  site: config.site.base_url,
+  base: config.site.base_path,
+  trailingSlash: config.site.trailing_slash,
   image: { service: sharp() },
   vite: { plugins: [tailwindcss()] },
   integrations: [
